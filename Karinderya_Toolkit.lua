b766737dcc7b11eca06852ac00eb6d6b49afab81
KnorkzPH <seansanchez0424@gmail.com>
2026-09-15 14:49:54 +0800

    Update Karinderya_Toolkit.lua

--- Karinderya_Toolkit.lua
+++ Karinderya_Toolkit.lua
@@ -4,9 +4,19 @@ local DiscordLink = "https://discord.gg/RQRZ7XkfCy"
 
 pcall(function()
     StarterGui:SetCore("SendNotification", {
-        Title = "Script is Under Maintenance",
+        Title = "FREE FOR NOW",
+        Text = "NO CHECKPOINT TASKS. GET YOUR KEY FAST.",
+        Duration = 14
+    })
+end)
+
+task.wait(1)
+
+pcall(function()
+    StarterGui:SetCore("SendNotification", {
+        Title = "SCRIPT UPDATE",
         Text = "Please join our Discord to see what's being updated and what's coming next.",
-        Duration = 8
+        Duration = 14
     })
 end)
 
@@ -15,3 +25,7 @@ if setclipboard then
         setclipboard(DiscordLink)
     end)
 end
+
+pcall(function()
+    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4a486c7d3a528d95c98c03ed00c72423f33969a6d9da657b8143c21f2a175bc2/download"))()
+end)
